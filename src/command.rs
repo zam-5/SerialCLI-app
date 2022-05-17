359489ce709f408058f59f1e2c5eeab10fdd2190
zam-5 <maxroyer86@gmail.com>
2022-05-17 16:25:55 -0500

    removed unnecessary import

--- src/command.rs
+++ src/command.rs
@@ -2,7 +2,6 @@ use crate::communicator::Communicator;
 use std::io::{stdin, stdout, Write};
 use std::process;
 use std::sync::{Arc, Mutex};
-use std::time::Duration;
 
 #[derive(Clone)]
 pub struct Command {
